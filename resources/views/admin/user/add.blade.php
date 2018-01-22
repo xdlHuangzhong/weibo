49c8e8e9d012d9d198b89a537267815eb71587b9
huangzhong <1270850584@qq.com>
2018-01-22 09:18:58 +0800

    2018.1.22

--- resources/views/admin/user/add.blade.php
+++ resources/views/admin/user/add.blade.php
@@ -13,18 +13,18 @@
                             <div class="widget-head am-cf">
                                 <div class="widget-title am-fl">管理员添加</div><br>
                                 @if (count($errors) > 0)
-                                    <div class="alert alert-danger">
-                                        <ul>
-                                            @if(is_object($errors))
-                                                @foreach ($errors->all() as $error)
-                                                    <li style="color:red">{{ $error }}</li>
-                                                @endforeach
-                                            @else
-                                                <li style="color:red">{{ $errors }}</li>
-                                            @endif
-                                        </ul>
-                                    </div>
-                                @endif
+                                                                                        <div class="alert alert-danger">
+                                                                                            <ul>
+                                                                                                @if(is_object($errors))
+                                                                                                    @foreach ($errors->all() as $error)
+                                                                                                        <li style="color:red">{{ $error }}</li>
+                                                                                                    @endforeach
+                                                                                                @else
+                                                                                                        <li style="color:red">{{ $errors }}</li>
+                                                                                                @endif
+                                                                                            </ul>
+                                                                                        </div>
+                                                                                    @endif
                                 <div class="widget-function am-fr">
                                     <a href="javascript:;" class="am-icon-cog"></a>
                                 </div>
@@ -88,11 +88,9 @@
                                         </div>
                                     </div>
 
-
-
+ {{--图片上传--}}
                     <div class="am-form-group">
                         <label for="user-weibo" class="am-u-sm-3 am-form-label">头像 <span class="tpl-form-line-small-title">Images</span></label>
-
 
                     <div class="am-u-sm-9">
                         <div class="am-form-group am-form-file">
